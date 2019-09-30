6600c0b1373675679c82dcb5efe25890ba0c1c00
KHart0012 <34316627+KHart0012@users.noreply.github.com>
2019-09-30 12:12:12 -0500

    Got basic functionality working

--- main.js
+++ main.js
@@ -1,27 +1,14 @@
-function calc_shortest(str1, str2, str3){
-    const l1 = str1.length
-    const l2 = str2.length
-    const l3 = str3.length
-
-    if (l1 <= l2 && l1 <= l3){
-        return str1
-    }
-    else if (l2 <= l1 && l2 <= l3){
-        return str2
-    }
-    else {
-        return str3
-    }
+function calc_numitems(numItems, numBags){
+    return numItems * numBags
 }
 
-function find_smallest() {
-    const str1 = document.querySelector("#input1").value
-    const str2 = document.querySelector("#input2").value
-    const str3 = document.querySelector("#input3").value
+function find_total_items() {
+    const items = document.querySelector("#input1").value
+    const bags = document.querySelector("#input2").value
 
-    const shortest = calc_shortest(str1, str2, str3)
+    const total_num = calc_numitems(items, bags)
 
-    document.querySelector('#result').innerHTML = shortest
+    document.querySelector('#result').innerHTML = total_num
 }
 
-document.querySelector('#submit').addEventListener('click', find_smallest)
+document.querySelector('#submit').addEventListener('click', find_total_items)
